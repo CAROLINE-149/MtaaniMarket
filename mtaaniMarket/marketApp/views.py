614783e017b544627d0dc52704aa506b527034a0
CAROLINE <caroonyango9@gmail.com>
2025-12-03 18:26:02 +0300

    User registration/login and basic frontend pages and navigation layout

--- mtaaniMarket/marketApp/views.py
+++ mtaaniMarket/marketApp/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+def home(request):
+    context ={}
+    return render(request,'marketApp/home.html',context)
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username)
+        print(password)
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print("User does not exists!")
+        
+        user = authenticate(request, username= username, password = password)
+
+        if user is not None: 
+            login(request, user)
+            return redirect('')
+        else:
+            print('Wrong Credentials!!')
+
+    context ={}
+    return render(request,'marketApp/login_form.html',context)
+
+def logoutUser(request):
+    context ={}
+    logout(request)
+    return redirect('home')
+
+def registerUser(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False) 
+            user.username = f"@{user.username}"
+            user.save()
+            login(request, user)
+            return redirect('')
+
+    context ={"form":form}
+    return render(request,'marketApp/register_form.html',context)
--- mtaaniMarket/mtaaniMarket/settings.py
+++ mtaaniMarket/mtaaniMarket/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'marketApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mtaaniMarket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
